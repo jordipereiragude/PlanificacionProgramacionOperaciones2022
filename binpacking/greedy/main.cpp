21a7ebe0a52d5ec39ff097c9f309f2fb1eb2f052
Jordi Pereira Gude <jordipereiragude@gmail.com>
2022-09-05 10:26:40 +0200

    first fit and best fit

--- binpacking/greedy/main.cpp
+++ binpacking/greedy/main.cpp
@@ -28,10 +28,64 @@ FILE* f;
 }
 
 int firstFit(instance* I) {
+  int remanente[I->n];
+  int solucion[I->n];
 
+  for(int i=0;i<I->n;i++) remanente[i]=I->c;
+  for(int i=0;i<I->n;i++) solucion[i]=(-1);
+  for(int i=0;i<I->n;i++) {
+    for(int j=0;j<I->n;j++) {
+      if(remanente[j]>=I->w[i]) {
+        remanente[j] = remanente[j] - I->w[i];
+        solucion[i]=j;
+        break;
+      }
+    }
+  }
+  int numBins=0;
+  for(int j=0;j<=I->n;j++) {
+    if(remanente[j]<I->c) {
+      numBins++;
+    }
+  }
+  for(int i=0;i<I->n;i++) {
+    printf("%d ",solucion[i]);
+  }
+  printf("firstFit usa %d cajas\n",numBins);
+  return 0;
 }
 
 int bestFit(instance* I) {
+  int remanente[I->n];
+  int solucion[I->n];
+  int mejorCaja;
+  int mejorCarga;
+
+  for(int i=0;i<I->n;i++) remanente[i]=I->c;
+  for(int i=0;i<I->n;i++) solucion[i]=(-1);
+  for(int i=0;i<I->n;i++) {
+    mejorCaja=(-1);
+    mejorCarga=I->c;
+    for(int j=0;j<I->n;j++) {
+      if((remanente[j]>=I->w[i])&&(mejorCarga>(remanente[j]-I->w[i]))) {
+          mejorCaja=j;
+          mejorCarga=remanente[j]-I->w[i];
+      }
+    }
+    remanente[mejorCaja] = mejorCarga;
+    solucion[i]=mejorCaja;
+  }
+  int numBins=0;
+  for(int j=0;j<=I->n;j++) {
+    if(remanente[j]<I->c) {
+      numBins++;
+    }
+  }
+  for(int i=0;i<I->n;i++) {
+    printf("%d ",solucion[i]);
+  }
+  printf("bestFit usa %d cajas\n",numBins);
+  return 0;
 }
 
 int main(int argc, char* argv[]) {
